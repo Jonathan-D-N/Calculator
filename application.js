f13745e7c86d60f783b983ca311e4c0884125d74
Jonathan <jdneave@gmail.com>
2022-12-15 16:35:01 -0700

    add multiply function

--- application.js
+++ application.js
@@ -3,11 +3,19 @@ function add () {
     for (let x of arguments) {
         result += x
     } return (result);
-}
+};
 
 function subtract () {
-    let result = (arguments[0])
-    for (let i = 1; i < arguments.length; i++) {
-        result -= arguments[i]
-    } return(result)
+        let result = (arguments[0]);
+        for (let i = 1; i < arguments.length; i++) {
+            result -= arguments[i]
+        } return(result)
+};
+
+function multiply (...numbers) {
+    if (numbers.length > 0) {
+    return numbers.reduce((product, number) => product * number, 1);
+    } else {
+        return 0;
+    }
 }
