840475f6da59810bad51b011eb1849a2ee39885d
Jonathan <jdneave@gmail.com>
2022-12-15 15:56:11 -0700

    subtract function

--- application.js
+++ application.js
@@ -5,3 +5,9 @@ function add () {
     } return (result);
 }
 
+function subtract () {
+    let result = (arguments[0])
+    for (let i = 1; i < arguments.length; i++) {
+        result -= arguments[i]
+    } return(result)
+}
