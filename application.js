3364f22faa9d4df02f4d64de4671580aea964bf4
Jonathan <jdneave@gmail.com>
2022-12-15 16:49:54 -0700

    refactor subtract to utilize .reduce

--- application.js
+++ application.js
@@ -5,12 +5,13 @@ function add () {
     } return (result);
 };
 
-function subtract () {
-        let result = (arguments[0]);
-        for (let i = 1; i < arguments.length; i++) {
-            result -= arguments[i]
-        } return(result)
-};
+function subtract (...numbers) {
+    if (numbers.length > 0) {
+        return numbers.reduce((product, number) => product - number)
+    } else {
+        return 0
+    }
+}
 
 function multiply (...numbers) {
     if (numbers.length > 0) {
