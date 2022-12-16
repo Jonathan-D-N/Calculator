da535cc4ea7e55cb89e9ec8e92cff150f71a8f6e
Jonathan <jdneave@gmail.com>
2022-12-15 17:15:43 -0700

    complete operate function

--- application.js
+++ application.js
@@ -32,9 +32,14 @@ function divide (...numbers) {
 
 function operate (num1, operator, num2) {
     if (operator == '+') {
-        console.log('test')
-        console.log(num1, num2)
-        return add(num1, num2)
+        return add(num1, num2);
+    } else if (operator == '-') {
+        return subtract(num1, num2);
+    } else if (operator == '*') {
+        return multiply(num1, num2);
+    } else if (operator == '/') {
+        return divide(num1, num2);
+    } else {
+        return('ERROR')
     }
-
 }
