bb2ca2b78b53ff339766c0fcd29163f92138a7e7
Jonathan-D-N <jdneave@gmail.com>
2022-12-20 13:30:54 -0700

    add preliminary eventlisteners

--- application.js
+++ application.js
@@ -24,3 +24,16 @@ const operators = {
 function operate (num1, num2) {
     return operators[key](num1, num2);
 }
+
+//
+const screenText = document.getElementById('screenText')
+const buttonSource = document.getElementById('wrapper');
+buttonSource.addEventListener('click', function(e) {
+    if (e.target.textContent == 'backspace') {
+        let tempText = screenText.textContent;
+        screenText.textContent = tempText.slice(0, -1)
+        console.log(tempText)
+    } else {
+    screenText.textContent += e.target.textContent;
+    }
+})
