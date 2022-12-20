e42dab5cefb2d64711200e662e9bf0d9db621e48
Jonathan-D-N <jdneave@gmail.com>
2022-12-20 10:44:40 -0700

    refactor operations to object

--- application.js
+++ application.js
@@ -1,45 +1,20 @@
-function add (...numbers) {
-    if (numbers.length > 0) {
-        return numbers.reduce((product, number) => product + number);
-    } else {
-        return 0
+const operations = {
+    '+': function(num1, num2) {
+        result =  num1 + num2;
+        return result;
+    },
+    '-': function(num1, num2) {
+        result =  num1 - num2;
+        return result;
+    },
+    '*': function(num1, num2) {
+        result =  num1 * num2;
+        return result;
+    },
+    '/': function(num1, num2) {
+        result = num1 / num2;
+        return result;
     }
 }
-
-function subtract (...numbers) {
-    if (numbers.length > 0) {
-        return numbers.reduce((product, number) => product - number);
-    } else {
-        return 0
-    }
-}
-
-function multiply (...numbers) {
-    if (numbers.length > 0) {
-    return numbers.reduce((product, number) => product * number);
-    } else {
-        return 0;
-    }
-}
-
-function divide (...numbers) {
-    if (numbers.length > 0) {
-    return numbers.reduce((product, number) => product / number);
-    } else {
-        return 0;
-    }
-}
-
-function operate (num1, operator, num2) {
-    if (operator == '+') {
-        return add(num1, num2);
-    } else if (operator == '-') {
-        return subtract(num1, num2);
-    } else if (operator == '*') {
-        return multiply(num1, num2);
-    } else if (operator == '/') {
-        return divide(num1, num2);
-    } else {
-        return('ERROR')
-    }
-}
+let result = '';
+let key = '';
