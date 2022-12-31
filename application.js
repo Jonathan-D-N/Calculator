aa5bcace9ede1640b38c3b0bf412b584edc8905d
Jonathan <jdneave@gmail.com>
2022-12-30 20:34:10 -0700

    reverting to commit 4a39d3b

--- application.js
+++ application.js
@@ -1,6 +1,4 @@
 //global variables
-let num1 = '';
-let num2 = '';
 let result = '';
 let key = '';
 
@@ -27,98 +25,78 @@ const operators = {
     }
 }
 
-function operate (num1, num2, operator) {
-    let key = operator;
+function operate (num1, num2) {
     return operators[key](num1, num2);
 }
 
 //
-const screenText = document.getElementById('screenText');
-const calculatorButtons = document.querySelectorAll('button');
-pressButtons()
-function pressButtons() {
-    for (const element of calculatorButtons) {
-        element.addEventListener('click', function(e) {
-            if (screenText.textContent.length < 20 && 
-            (e.target.classList.contains('calculatorNumber') || e.target.classList.contains('printable'))) {
-                displayButtons(e.target.textContent);
-                // screenText.textContent += e.target.textContent;
-            }
-        });
-    }
+// event listeners
+let leftText = ''
+let rightText = ''
+const screenText = document.getElementById('screenText')
+//numbers
+const numberButtons = document.querySelectorAll('.calculatorNumber')
+for (const element of numberButtons) {
+    element.addEventListener('click', function(e) {
+        if (screenText.textContent.length < 20) {
+        screenText.textContent += e.target.textContent;
 }
-
-function displayButtons(e) {
-    if (e == '÷' && screenText.textContent.includes('÷')
-        || e == '*' && screenText.textContent.includes('*')
-        || e == '-' && screenText.textContent.includes('-')
-        || e == '+' && screenText.textContent.includes('+')){
-        return;
-    }
-     screenText.textContent += e;
-     console.log(e)
+});
 }
-
-function operatorButtons(e) {
-        if (e.target.textContent === 'backspace') {
+const buttonFunctions = document.getElementById('container')
+//backspace and delete
+buttonFunctions.addEventListener('click', function(e) {
+    if (e.target.textContent === 'backspace') {
         let str = screenText.textContent;
         screenText.textContent = str.slice(0, -1)
+    } else if (e.target.textContent === 'delete') {
+        screenText.textContent = '';
+        return;
+    } //operators
+      else if (e.target.textContent === '÷' && screenText.textContent > 0) {
+        key = '/'
+        screenText.textContent += e.target.textContent
+    } else if (e.target.textContent === '*' && screenText.textContent > 0) {
+        key = '*'
+        screenText.textContent += e.target.textContent
+    } else if (e.target.textContent === '-' && screenText.textContent > 0) {
+        key = '-'
+        screenText.textContent += e.target.textContent
+    } else if (e.target.textContent === '+' && screenText.textContent > 0) {
+        key = '+'
+        screenText.textContent += e.target.textContent
+    }
+});
+//equals operator
+const equalsButton = document.getElementById('equals');
+equalsButton.addEventListener('click', function(element) {
+    splitEquals(screenText.textContent)
+});
+let op1 = 0
+let op2 = 0
+function splitEquals() {
+    let str = screenText.textContent;
+    let arr = str.split('')
+    let results = [];
+    let currentNum = '';
+if (screenText.textContent.length > 0) {
+    for(let i = 0; i < arr.length; i++) {
+        let char = arr[i];
+        if (/[a-zA-Z0-9]/.test(char)){
+            currentNum += char;
+        } else {
+            if(currentNum.length > 0) {
+                results.push(currentNum);
+                currentNum = '';
+            }
+            results.push(char);
+        }
+    }
+    if(currentNum.length > 0) {
+        results.push(currentNum);
+    }
+    op1 = Number(results[0]);
+    op2 = Number(results[2]);
+    operate(op1, op2);
 }
 }
-
-// const functionButtons = document.getElementById('container')
-// functionButtons.addEventListener('click', function(e) {
-//     if (e.target.textContent === 'backspace') {
-//         let str = screenText.textContent;
-//         screenText.textContent = str.slice(0, -1)
-//     } else if (e.target.textContent === 'delete') {
-//         screenText.textContent = '';
-//         return;
-//     } //operators
-//       else if (e.target.textContent === '÷' && screenText.textContent > 0) {
-//         key = '/'
-//         screenText.textContent += e.target.textContent
-//     } else if (e.target.textContent === '*' && screenText.textContent > 0) {
-//         key = '*'
-//         screenText.textContent += e.target.textContent
-//     } else if (e.target.textContent === '-' && screenText.textContent > 0) {
-//         key = '-'
-//         screenText.textContent += e.target.textContent
-//     } else if (e.target.textContent === '+' && screenText.textContent > 0) {
-//         key = '+'
-//         screenText.textContent += e.target.textContent
-//     }
-// });
-// //equals operator
-// const equalsButton = document.getElementById('equals');
-// equalsButton.addEventListener('click', function(element) {
-//     splitEquals(screenText.textContent)
-// });
-// let op1 = 0
-// let op2 = 0
-// function splitEquals() {
-//     let str = screenText.textContent;
-//     let arr = str.split('')
-//     let results = [];
-//     let currentNum = '';
-// if (screenText.textContent.length > 0) {
-//     for(let i = 0; i < arr.length; i++) {
-//         let char = arr[i];
-//         if (/[a-zA-Z0-9]/.test(char)){
-//             currentNum += char;
-//         } else {
-//             if(currentNum.length > 0) {
-//                 results.push(currentNum);
-//                 currentNum = '';
-//             }
-//             results.push(char);
-//         }
-//     }
-//     if(currentNum.length > 0) {
-//         results.push(currentNum);
-//     }
-//     op1 = Number(results[0]);
-//     op2 = Number(results[2]);
-//     operate(op1, op2);
-// }
-// }
