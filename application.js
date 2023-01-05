dd8c5af16ea0a279604e0b270d6a0aac6265e3a8
Jonathan <jdneave@gmail.com>
2023-01-04 19:30:20 -0700

    Input logging to 2 global variables, Operator checker added

--- application.js
+++ application.js
@@ -1,6 +1,28 @@
 //global variables
 let result = '';
 let key = '';
+let input1 = '';
+let input2 = '';
+
+function inputSelector(num1, num2) {
+    input1 = num1;
+    console.log('input1: ', input1);
+    input2 = num2;
+    console.log('input2: ', input2)
+}
+let operatorStatus = false
+function checkForOperator() {
+    if (screenText.textContent.includes("+") ||
+        screenText.textContent.includes("-") ||
+        screenText.textContent.includes("*") ||
+        screenText.textContent.includes("÷")) {
+        console.log('true')
+        operatorStatus = true
+    } else {
+        console.log('false')
+        operatorStatus = false
+    }
+}
 
 const operators = {
     '+': function(num1, num2) {
@@ -29,69 +51,83 @@ function operate (num1, num2) {
     return operators[key](num1, num2);
 }
 
+//number buttons
 const screenText = document.getElementById('screenText')
 const numberButtons = document.querySelectorAll('.calculatorNumber')
     for (const element of numberButtons) {
-        element.addEventListener('click', function(e) {
+        element.addEventListener('click', function(e) {    
+            checkForOperator()        
             if (screenText.textContent.length < 20) {
+                if (operatorStatus == false)   { 
                 screenText.textContent += e.target.textContent;
+                input1 += (e.target.textContent);
+                inputSelector(input1, input2);
+                } else {
+                    screenText.textContent += e.target.textContent;
+                    input2 += (e.target.textContent);
+                    inputSelector(input1, input2);
+                }
             }
         });
     }
-const buttonFunctions = document.getElementById('container')
-//backspace and delete
-buttonFunctions.addEventListener('click', function(e) {
-    if (e.target.textContent === 'backspace') {
+//backspace button
+const backspaceButton = document.getElementById('backspace');
+    backspaceButton.addEventListener('click', function() {
+        if (operatorStatus == false) {
         let str = screenText.textContent;
-        screenText.textContent = str.slice(0, -1)
-    } else if (e.target.textContent === 'delete') {
-        screenText.textContent = '';
-        return;
-    } //operators
-      else if (e.target.textContent === '÷' && screenText.textContent > 0) {
-        key = '/'
-        screenText.textContent += e.target.textContent
-    } else if (e.target.textContent === '*' && screenText.textContent > 0) {
-        key = '*'
-        screenText.textContent += e.target.textContent
-    } else if (e.target.textContent === '-' && screenText.textContent > 0) {
-        key = '-'
-        screenText.textContent += e.target.textContent
-    } else if (e.target.textContent === '+' && screenText.textContent > 0) {
-        key = '+'
-        screenText.textContent += e.target.textContent
-    }
-});
-//equals operator
-const equalsButton = document.getElementById('equals');
-equalsButton.addEventListener('click', function(element) {
-    splitEquals(screenText.textContent)
-});
-let op1 = 0
-let op2 = 0
-function splitEquals() {
-    let str = screenText.textContent;
-    let arr = str.split('')
-    let results = [];
-    let currentNum = '';
-if (screenText.textContent.length > 0) {
-    for(let i = 0; i < arr.length; i++) {
-        let char = arr[i];
-        if (/[a-zA-Z0-9]/.test(char)){
-            currentNum += char;
-        } else {
-            if(currentNum.length > 0) {
-                results.push(currentNum);
-                currentNum = '';
-            }
-            results.push(char);
+        screenText.textContent = str.slice(0, -1);
+        input1 = input1.slice(0, -1);
         }
-    }
-    if(currentNum.length > 0) {
-        results.push(currentNum);
-    }
-    op1 = Number(results[0]);
-    op2 = Number(results[2]);
-    operate(op1, op2);
-}
-}
+        else {
+            let str = screenText.textContent;
+            screenText.textContent = str.slice(0, -1);
+            input2 = input2.slice(0, -1);
+            checkForOperator();
+        }
+    });
+
+//clear button
+const clearButton = document.getElementById('clear');
+    clearButton.addEventListener('click', function() {
+        screenText.textContent = '';
+        input1 = '';
+        input2 = '';
+        checkForOperator();
+    });
+
+//operator buttons
+const additionButton = document.getElementById('addition');
+    additionButton.addEventListener('click', function(e) {
+        if (screenText.textContent > 0) {
+            checkForOperator();
+            key = '+';
+            screenText.textContent += '+';
+        }
+    });
+
+const subtractButton = document.getElementById('subtract');
+    subtractButton.addEventListener('click', function(e) {
+        if (screenText.textContent > 0) {
+            checkForOperator();
+            key = '-';
+            screenText.textContent += '-';
+        }
+    });
+
+const multiplyButton = document.getElementById('multiply');
+    multiplyButton.addEventListener('click', function(e) {
+        if (screenText.textContent > 0) {
+            checkForOperator();
+            key = '*';
+            screenText.textContent += '*';
+        }
+    });
+
+const divideButton = document.getElementById('divide');
+    divideButton.addEventListener('click', function(e) {
+        if (screenText.textContent > 0) {
+            checkForOperator();
+            key = '÷';
+            screenText.textContent += '÷';
+        }
+    });
