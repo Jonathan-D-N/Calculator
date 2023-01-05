052493edad84841a2c6984bd49ca4ebb70105129
Jonathan <jdneave@gmail.com>
2023-01-04 20:25:10 -0700

    add clearEqualsText function. removed console.log

--- application.js
+++ application.js
@@ -39,19 +39,20 @@ function checkForOperator() {
         screenText.textContent.includes("*") ||
         screenText.textContent.includes("÷")) {
         operatorStatus = true
-        console.log(operatorStatus)
     } else {
         operatorStatus = false
-        console.log(operatorStatus)
     }
 }
-
+function clearEqualsText() {
+    equalsScreenText.textContent = ''
+}
 //number buttons
 const screenText = document.getElementById('screenText')
 const numberButtons = document.querySelectorAll('.calculatorNumber')
     for (const element of numberButtons) {
         element.addEventListener('click', function(e) {    
-            checkForOperator()        
+            checkForOperator();
+            clearEqualsText();    
             if (screenText.textContent.length < 20) {
                 if (operatorStatus == false)   { 
                 screenText.textContent += e.target.textContent;
@@ -66,6 +67,7 @@ const numberButtons = document.querySelectorAll('.calculatorNumber')
 //backspace button
 const backspaceButton = document.getElementById('backspace');
     backspaceButton.addEventListener('click', function() {
+        clearEqualsText(); 
         if (operatorStatus == false) {
         let str = screenText.textContent;
         screenText.textContent = str.slice(0, -1);
@@ -82,6 +84,7 @@ const backspaceButton = document.getElementById('backspace');
 //clear button
 const clearButton = document.getElementById('clear');
     clearButton.addEventListener('click', function() {
+        clearEqualsText(); 
         screenText.textContent = '';
         input1 = '';
         input2 = '';
@@ -89,9 +92,10 @@ const clearButton = document.getElementById('clear');
     });
 
 //operator buttons
-const additionButton = document.querySelectorAll('.operator');
-    for (const element of additionButton) {
+const operatorButtons = document.querySelectorAll('.operator');
+    for (const element of operatorButtons) {
         element.addEventListener('click', function(e) {
+            clearEqualsText(); 
             if (screenText.textContent > 0) {
                 key = e.target.textContent;
                 screenText.textContent += e.target.textContent;
@@ -104,6 +108,7 @@ const additionButton = document.querySelectorAll('.operator');
 const equalsScreenText = document.getElementById('equalsScreenText')
 const equalsButton = document.getElementById('equals');
     equalsButton.addEventListener('click', function(e) {
+        clearEqualsText(); 
         operate(~~input1, ~~input2)
         equalsScreenText.textContent = operate(~~input1, ~~input2);
     });
